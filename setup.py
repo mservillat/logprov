785cd8f974a67941d1415c256d64cb1672a43a24
mservillat <mathieu.servillat@obspm.fr>
2020-07-28 15:25:52 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="logprov",
+    version="0.0.1",
+    author="Mathieu Servillat & Jose Enrique Ruiz",
+    author_email="mathieu.servillat@obspm.fr, jer@iaa.es",
+    description="capture of provenance information into structured logs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mservillat/logprov",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
