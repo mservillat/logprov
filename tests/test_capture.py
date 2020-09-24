4760d024ad3f47c27ddb0f1a6ed3ca607e0c2911
mservillat <mathieu.servillat@obspm.fr>
2020-09-24 14:24:03 +0200

    add a test on files

--- tests/test_capture.py
+++ tests/test_capture.py
@@ -2,6 +2,7 @@ import logprov.capture
 from logprov.io import read_prov, provlist2provdoc, provdoc2svg
 import yaml
 import datetime
+from shutil import copyfile
 
 provconfig = {
     'capture': True,
@@ -35,6 +36,40 @@ activity_description:
         generation:
             - role: object2
               description: "output object"
+              entity_description: Object
+              value: object2
+    write:
+        description: "write object1 and object2 in a text file"
+        parameters:
+            - name: filename
+              description: "output file name"
+              value: kwargs.filename
+        usage:
+            - role: object1
+              entity_description: Object
+              value: object1
+            - role: object2
+              entity_description: Object
+              value: object2
+        generation:
+            - role: text file
+              entity_description: File
+              location: kwargs.filename
+    read:
+        description: "read object1 and object2 from a text file"
+        parameters:
+            - name: filename
+              description: "input file name"
+              value: kwargs.filename
+        usage:
+            - role: text file
+              entity_description: File
+              location: kwargs.filename
+        generation:
+            - role: object1
+              entity_description: Object
+              value: object1
+            - role: object2
               entity_description: Object
               value: object2
 entity_description:
@@ -82,16 +117,34 @@ class Class1(object):
         self.object1.value = value
         return self.object1
 
+    def write(self, filename="prov_test.txt"):
+        with open(filename, "w") as f:
+            f.write(f"A={self.object1} B={self.object2}")
+
+    def read(self, filename="prov_test.txt"):
+        with open(filename, "r") as f:
+            line = f.read()
+        for item in line.split(" "):
+            name, value = item.split("=")
+            if name == "A":
+                self.object1.value = value
+            if name == "B":
+                self.object2.value = value
+
 
 start = datetime.datetime.now().isoformat()
 c1 = Class1()
-c1.function1(value=2)
+c1.function1(value=1)
 #c1.function1(value=2)
 #c1.function3(value=5)
-c1.function2(value=2)
+c1.function2(value=3)
+c1.object1.value = 5
 #c1.function3(value=2)
 c1.function1(value=2)
 c1.function2(value=2)
+c1.write(filename="prov_test1.txt")
+copyfile("prov_test1.txt", "prov_test2.txt")
+c1.read(filename="prov_test2.txt")
 end = datetime.datetime.now().isoformat()
 
 logname = provconfig['log_filename']
