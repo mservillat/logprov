44c7ed1dac0dccf4622eb65507c7dc6ad0fdb5f4
mservillat <mathieu.servillat@obspm.fr>
2021-05-26 16:54:15 +0200

    v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="logprov",
-    version="0.0.1",
+    version="0.0.2",
     author="Mathieu Servillat & Jose Enrique Ruiz",
     author_email="mathieu.servillat@obspm.fr, jer@iaa.es",
     description="capture of provenance information into structured logs",
